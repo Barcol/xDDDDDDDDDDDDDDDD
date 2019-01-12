d566f923bb794ab1246b69feb9c4f48e02d15118
AHercog <anubarak123@gmail.com>
2019-01-12 19:26:45 +0100

    end points added

--- src/main.py
+++ src/main.py
@@ -1,6 +1,4 @@
-from typing import Tuple
-
-from flask import Flask
+from flask import Flask, request, make_response
 
 from src.placed_plants import PlacedPlants
 from src.plants_database import Database
@@ -11,13 +9,27 @@ placed_plants = PlacedPlants()
 
 
 @app.route("/addPlant", methods=["GET"])
-def add_plant(name: str, position: Tuple):
-    placed_plants.add_plant(plants_database.find_plant(name), position)
+def add_plant():
+    arguments = request.args
+    name = arguments.get("name")
+    position_x = int(arguments.get("positionX"))
+    position_y = int(arguments.get("positionY"))
+    placed_plants.add_plant(plants_database.find_plant(name), (position_x, position_y))
+    print("wykonano funkcje 1")
 
+    resp = make_response("")
+    return resp
 
 @app.route("/getPlantsList", methods=["GET"])
-def get_plants(user_position, distance):
-    placed_plants.show_plants_in_range(user_position, distance)
+def get_plants():
+    arguments = request.args
+    user_position_x = int(arguments.get("userPositionX"))
+    user_position_y = int(arguments.get("userPositionY"))
+    distance = arguments.get("distance")
+    placed_plants.show_plants_in_range((user_position_x, user_position_y), distance)
+    print("wykonano funkcje 2")
 
+    resp = make_response("")
+    return resp
 
-print(placed_plants.show_plants_in_range((15, 15), 4))
+app.run("0.0.0.0", 5000)
