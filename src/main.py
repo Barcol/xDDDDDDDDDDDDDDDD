fe3e1ab3a64cf8805c8ec7dfa4d2833c98ae288e
AHercog <anubarak123@gmail.com>
2019-01-12 20:32:17 +0100

    working better now

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, make_response
+from flask.json import jsonify
 
 from src.placed_plants import PlacedPlants
 from src.plants_database import Database
@@ -12,24 +13,26 @@ placed_plants = PlacedPlants()
 def add_plant():
     arguments = request.args
     name = arguments.get("name")
-    position_x = int(arguments.get("positionX"))
-    position_y = int(arguments.get("positionY"))
+    position_x = float(arguments.get("positionX"))
+    position_y = float(arguments.get("positionY"))
     placed_plants.add_plant(plants_database.find_plant(name), (position_x, position_y))
     print("wykonano funkcje 1")
 
     resp = make_response("")
     return resp
 
+
 @app.route("/getPlantsList", methods=["GET"])
 def get_plants():
     arguments = request.args
-    user_position_x = int(arguments.get("userPositionX"))
-    user_position_y = int(arguments.get("userPositionY"))
-    distance = arguments.get("distance")
-    placed_plants.show_plants_in_range((user_position_x, user_position_y), distance)
+    user_position_x = float(arguments.get("userPositionX"))
+    user_position_y = float(arguments.get("userPositionY"))
+    distance = float(arguments.get("distance"))
+    resp = jsonify(placed_plants.show_plants_in_range((user_position_x, user_position_y), distance))
     print("wykonano funkcje 2")
 
-    resp = make_response("")
+    # resp = make_response("")
     return resp
+
 
 app.run("0.0.0.0", 5000)
--- src/placed_plants.py
+++ src/placed_plants.py
@@ -1,12 +1,12 @@
-from typing import Tuple, List
+from typing import Tuple, Dict
 
 
 class PlacedPlants:
     def __init__(self):
-        self.__placed_plants_data = []
+        self.__placed_plants_data = {}
 
     def add_plant(self, name: str, position: Tuple[float, float]):
-        self.__placed_plants_data.append((name, position))
+        self.__placed_plants_data[name] = position
 
     @staticmethod
     def is_plant_in_range(user_position: Tuple[float, float], plant_position: Tuple[float, float],
@@ -15,11 +15,11 @@ class PlacedPlants:
             if abs(user_position[1] - plant_position[1]) < distance:
                 return True
 
-    def show_plants_in_range(self, user_position: Tuple[float, float], distance: float) -> List:
-        plants_to_return = []
+    def show_plants_in_range(self, user_position: Tuple[float, float], distance: float) -> Dict:
+        plants_to_return = {}
         if distance == 0:
             return self.__placed_plants_data
         for plant in self.__placed_plants_data:
-            if self.is_plant_in_range(user_position, plant[1], distance):
-                plants_to_return.append(plant)
+            if self.is_plant_in_range(user_position, self.__placed_plants_data[plant], distance):
+                plants_to_return[plant] = self.__placed_plants_data[plant]
         return plants_to_return
